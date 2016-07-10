cf4b1ef256c7658f0d75f418224bcf5f916d1fd0
MajorTom327 <majortom@d-society.fr>
2016-07-10 06:35:38 +0200

    add some change about the color system

--- src/f_print.c
+++ src/f_print.c
@@ -6,38 +6,45 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/03 07:49:38 by vthomas           #+#    #+#             */
-/*   Updated: 2016/06/18 19:32:03 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/07/10 04:46:40 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <sys/stat.h>
+#include <debug.h>
 
-static char *sf_getcolor(t_dirent file)
+static char *sf_getcolor(const char *file)
 {
-	const int type = file.d_type;
+	//const int type = file.d_type;
+	struct stat *buf;
+	int type;
 
-	if (type == DT_FIFO)
-		return ("\033[32m");
-	if (type == DT_CHR)
-		return ("\033[33m");
-	if (type == DT_DIR)
+	buf = NULL;
+	lstat(file, &buf);
+	if (buf == NULL)
+		exit(EXIT_FAILURE);
+	type = buf->st_mode;
+	dbg_info("sf_getcolor", "In the function", 3);
+	dbg_var_int("sf_getcolor","FILE VALUE", type, 3);
+//	if (type == S_IFMT)
+//		return ("\033[31m");
+	if (type == S_IFDIR)
 		return ("\033[34m");
-	if (type == DT_BLK)
+	if (type == S_IFBLK)
 		return ("\033[35m");
-	if (type == DT_REG)
+	if (type == S_IFREG)
 		return ("\033[36m");
-	if (type == DT_LNK)
+	if (type == S_IFLNK)
 		return ("\033[37m");
-	if (type == DT_SOCK)
+	if (type == S_IFSOCK)
 		return ("\033[38m");
-	if (type == DT_WHT)
+	if (type == S_IFWHT)
 		return ("\033[39m");
-	if (type == DT_UNKNOWN)
-			return ("\033[0m");
 	return ("\033[90m");
 }
 
-static void sf_print(t_dirent f, int arg)
+static void sf_print(t_dirent f, int arg, const char *dir)
 {
 	char *str;
 
@@ -47,12 +54,13 @@ static void sf_print(t_dirent f, int arg)
 	if (str == NULL)
 		return ;
 	if (arg & 0x10)
-		ft_strcat(str, sf_getcolor(f));
+		ft_strcat(str, sf_getcolor(dir));
 	ft_strcat(str, f.d_name);
 	if (arg & 0x10)
 		ft_strcat(str, "\033[0m");
-	ft_putstr(str);
-	ft_putstr("\t\t");
+//	ft_putstr(str);
+//	ft_putstr("\t\t");
+	ft_putendl(str);//TODO: ENDL AUTO
 	ft_strdel(&str);
 }
 
@@ -75,7 +83,7 @@ void	print_file(t_file *f_list, int size, int arg)
 		}
 		while (i < f_list[j].nb)
 		{
-			sf_print(*f_list[j].f_list[i], arg);
+			sf_print(*f_list[j].f_list[i], arg, f_list[j].name);
 			//ft_putstr(f_list[j].f_list[i]->d_name);
 			if (!((i + 1) % 5))
 				ft_putendl("");
